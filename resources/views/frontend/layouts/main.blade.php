f99d5bccf06e2711e47bfbbfae4e4583c2654a7e
Ahsin Javaid <52376720+ahsinjavaid890@users.noreply.github.com>
2024-02-16 00:10:29 +0500

    hjkhkj

--- resources/views/frontend/layouts/main.blade.php
+++ resources/views/frontend/layouts/main.blade.php
@@ -1,90 +1,81 @@
 <!doctype html>
-<html lang="zxx">
+<html lang="en">
+
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
-	<!-- <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> -->
-	<link rel="stylesheet" href="{{ asset('public/front/css/bootstrap.min.css') }}">
-	<link rel="stylesheet" href="{{ asset('public/front/css/owl.theme.default.min.css') }}">
-	<link rel="stylesheet" href="{{ asset('public/front/css/owl.carousel.min.css') }}">
-	<link rel="stylesheet" href="{{ asset('public/front/css/magnific-popup.css') }}">
-	<link rel="stylesheet" href="{{ asset('public/front/css/animate.css') }}">
-	<link rel="stylesheet" href="{{ asset('public/front/css/boxicons.min.css') }}">
-	<link rel="stylesheet" href="{{ asset('public/front/css/flaticon.css') }}">
-	<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/jquery.slick/1.4.1/slick.css">
-	<link rel="stylesheet" href="{{ asset('public/front/css/meanmenu.css') }}">
-	<link rel="stylesheet" href="{{ asset('public/front/css/nice-select.css') }}">
-	<link rel="stylesheet" href="{{ asset('public/front/css/odometer.css') }}">
-	<link rel="stylesheet" href="{{ asset('public/front/css/style.css') }}">
-	<link rel="stylesheet" href="{{ asset('public/front/css/time.css') }}">
-	<link rel="stylesheet" href="{{ asset('public/front/css/responsive.css') }}">
-	<script src="{{ asset('public/front/js/jquery-3.5.1.slim.min.js') }}"></script>
-	<meta name="csrf-token" content="{{ csrf_token() }}">
-	<link rel="icon" type="image/png" href="{{ url('public/images') }}/{{ Cmf::get_store_value('favicon') }}">    
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="{{ url('public/front/css/bootstrap.min.css') }}" rel="stylesheet">
+
+    <link href="{{ url('public/front/css/bootstrap-icons.css') }}" rel="stylesheet">
+
+    <link href="{{ url('public/front/css/all.min.css') }}" rel="stylesheet">
+
+    <link href="{{ url('public/front/css/fontawesome.min.css') }}" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{ url('public/front/css/swiper-bundle.min.css') }}">
+
+    <link rel="stylesheet" href="{{ url('public/front/css/animate.min.css') }}">
+    <link rel="stylesheet" href="{{ url('public/front/css/jquery.fancybox.min.css') }}">
+
+    <link href="{{ url('public/front/css/boxicons.min.css') }}" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{ url('public/front/css/preloader.css') }}">
+    <link rel="stylesheet" href="{{ url('public/front/css/style2.css') }}">
+
+    <link rel="icon" href="{{ url('public/front/img/sm-logo..svg') }}" type="image/gif" sizes="20x20">
   	@yield('tittle')
-  	<style type="text/css">
-  		/*#loading {
-				  position: fixed;
-				  display: block;
-				  width: 100%;
-				  height: 100%;
-				  top: 0;
-				  left: 0;
-				  text-align: center;
-				  opacity: 1;
-				  background-color: #dddd;
-				  z-index: 99;
-				}
-				.loading-div{
-					position: absolute;
-			    left: 35%;
-			    top: 50%;
-			    z-index: 100;
-			    background-color: #3a5371;
-			    padding: 10px;
-			    border-radius: 15px;
-				}
-				.loading-image{
-					margin-bottom: 15px;
-			    background-color: white;
-			    border-radius: 10px;
-			    padding: 10px;
-				}*/
-  	</style>
 </head>
 	<body class="body-five">
-		<!-- <div id="loading">
-			<div class="loading-div">
-				<img id="loading-image" src="{{ url('public/images/118135255.png') }}" alt="Loading..." />
-				<h1>Finding the best plans for you</h1>
-			</div>
-		  
-		</div> -->
 		@include('frontend.includes.header')
 			@yield('content')
 		@include('frontend.includes.footer')
-		<script src="{{ asset('public/front/js/popper.min.js') }}"></script>
-		<script src="{{ asset('public/front/js/bootstrap.min.js') }}"></script>
-		<script src="{{ asset('public/front/js/jquery.meanmenu.js') }}"></script>
-		<script src="{{ asset('public/front/js/wow.min.js') }}"></script>
-		<script src="{{ asset('public/front/js/owl.carousel.js') }}"></script>
-		<script src="{{ asset('public/front/js/jquery.magnific-popup.min.js') }}"></script>
-		<script src="{{ asset('public/front/js/parallax.min.js') }}"></script>
-		<script src="{{ asset('public/front/js/jquery.mixitup.min.js') }}"></script>
-		<script src="{{ asset('public/front/js/jquery.appear.js') }}"></script>
-		<script src="{{ asset('public/front/js/odometer.min.js') }}"></script>
-		<script src="{{ asset('public/front/js/jquery.ajaxchimp.min.js') }}"></script>
-		<script src="{{ asset('public/front/js/form-validator.min.js') }}"></script>
-		<script src="{{ asset('public/front/js/contact-form-script.js') }}"></script>
-		<script src="{{ asset('public/front/js/custom.js') }}"></script>
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.5.5/slick.min.js"></script>
-		<div class="compare_header_top">
+		<script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+    <script src="{{ url('public/front/js/jquery-3.6.0.min.js') }}"></script>
 
-		</div>
-	</body>
+    <script src="{{ url('public/front/js/popper.min.js') }}"></script>
+    <script src="{{ url('public/front/js/bootstrap.min.js') }}"></script>
+
+    <script src="{{ url('public/front/js/swiper-bundle.min.js') }}"></script>
+
+    <script src="{{ url('public/front/js/waypoints.min.js') }}"></script>
+
+    <script src="{{ url('public/front/js/jquery.counterup.min.js') }}"></script>
+
+    <script src="{{ url('public/front/js/isotope.pkgd.min.js') }}"></script>
+
+    <script src="{{ url('public/front/js/jquery.fancybox.min.js') }}"></script>
+
+    <script src="{{ url('public/front/js/gsap.min.js') }}"></script>
+    <script src="{{ url('public/front/js/simpleParallax.min.js') }}"></script>
+    <script src="{{ url('public/front/js/TweenMax.min.js') }}"></script>
+
+    <script src="{{ url('public/front/js/jquery.marquee.min.js') }}"></script>
+
+    <script src="{{ url('public/front/js/wow.min.js') }}"></script>
+
+    <script src="{{ url('public/front/js/preloader.js') }}"></script>
+    <script src="{{ url('public/front/js/custom.js') }}"></script>
+    <script>
+        $(".marquee_text").marquee({
+            direction: "left",
+            duration: 25000,
+            gap: 50,
+            delayBeforeStart: 0,
+            duplicated: true,
+            startVisible: true,
+        });
+        $(".marquee_text2").marquee({
+            direction: "top",
+            duration: 25000,
+            gap: 50,
+            delayBeforeStart: 0,
+            duplicated: true,
+            startVisible: true,
+        });
+    </script>
+    @yield('script')
+</body>
+
 </html>
-@yield('script')
